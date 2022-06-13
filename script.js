bad5f1449aeb557b5450a16f56ae9999a9dde200
leninepestana <leninepestana@gmail.com>
2022-06-13 22:23:14 +0100

    Math.random

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 17;
+
+document.querySelector('.guess').value = '26';
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent =
+            'Too high! The number is smaller';
+    } else {
+        document.querySelector('.message').textContent =
+            'Too small. The number is larger';
+    }
+});
