a6b0ff5b00959e26e859c9f16804f45898350c5e
leninepestana <leninepestana@gmail.com>
2022-06-14 06:57:33 +0100

    Implemeting the logic game

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-//document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -23,10 +24,26 @@ document.querySelector('.check').addEventListener('click', function () {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent =
-            'Too high! The number is smaller';
-    } else {
-        document.querySelector('.message').textContent =
-            'Too small. The number is larger';
+        if (score > 1) {
+            document.querySelector('.message').textContent =
+                'Too high! Guess is smaller';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                '😩 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent =
+                'Too small. Guess is larger';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                '😩 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
