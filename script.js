96a111f4056752e55bca35f2de208cf9eb10df6c
leninepestana <leninepestana@gmail.com>
2022-06-14 13:26:34 +0100

    Manipulating CSS

--- script.js
+++ script.js
@@ -19,14 +19,21 @@ document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess, guess);
 
+    // When theres no input
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number!';
+        // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        // When guess is to high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent =
-                'Too high! Guess is smaller';
+                'Too high! Guess is more low';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
@@ -34,10 +41,11 @@ document.querySelector('.check').addEventListener('click', function () {
                 '😩 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+        // When guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent =
-                'Too small. Guess is larger';
+                'Too low. Guess is more higher';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
